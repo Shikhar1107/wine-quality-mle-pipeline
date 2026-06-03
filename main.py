44930e75203f93cdf3043ff6d2b4d21c2a3c8003
Shikhar Gupta <shikharjul01@gmail.com>
2026-06-03 16:05:13 +0530

    custom logging added

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.wine_quality_mle_pipeline import logger
+
+logger.info("Welcome to our custom project logger")
--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name ="wine-quality-mle-pipeline"
+project_name ="wine_quality_mle_pipeline"
 
 list_of_files =[
     '.github/workflows/.gitkeep',
